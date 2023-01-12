7d87212f73863c9b8c2eff0d45a9b639c1d0c73e
Topias Mariapori <topias@mariapori.fi>
2023-01-12 22:22:38 +0200

    SlashCommandit

--- masa/Program.cs
+++ masa/Program.cs
@@ -11,14 +11,49 @@ public class Program
     public async Task MainAsync(string[] args) { 
         _client = new DiscordSocketClient();
         _client.Log += Log;
+        _client.Ready += _client_Ready;
+        _client.SlashCommandExecuted += _client_SlashCommandExecuted;
         _token = args[0] ?? throw new NullReferenceException("Sinulla pitää olla bot-token!");
         
         await _client.LoginAsync(TokenType.Bot, _token);
         await _client.StartAsync();
-        await SetInterval(() => LaitaKissakuva(), TimeSpan.FromHours(12),true);
 
         await Task.Delay(-1);
     }
+
+    private async Task _client_SlashCommandExecuted(SocketSlashCommand arg)
+    {
+        switch(arg.Data.Name)
+        {
+            case "info":
+                await arg.RespondAsync("Masa on Latarin kissa. Minut on luonut @Mariapori#5965 ja minuun voi ehdottaa lisää komentoja githubissa.");
+                break;
+        }
+    }
+
+    private async Task _client_Ready()
+    {
+        await SetInterval(() => LaitaKissakuva(), TimeSpan.FromHours(12), true);
+        
+        var guild = _client?.GetGuild(252154029868711937);
+        
+        if (guild != null)
+        {
+            var infoCmd = new SlashCommandBuilder();
+            infoCmd.WithName("info");
+            infoCmd.WithDescription("Tällä komennolla saat tietoja Masasta ja sen kehittäjistä.");
+
+            try
+            {
+                await guild.CreateApplicationCommandAsync(infoCmd.Build());
+
+            }catch(Exception ex) 
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+
     private Task Log(LogMessage msg)
     {
         Console.WriteLine(msg.ToString());
@@ -43,8 +78,6 @@ public class Program
     {
         if (firstTime)
         {
-            //Botti lähettää käynnistymisessä ekan kuvan ja odottaa 10 sekuntia että on kerinnyt kirjautumaan.
-            await Task.Delay(10000).ConfigureAwait(false);
             action();
         }
 
